6fd49954a984e3490079a8f7b0c998e49697ea35
mo7amed4a <m7md0a99@gmail.com>
2025-05-29 15:23:35 +0300

    Edit edit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -15,16 +15,7 @@ export default [
       },
     },
   },
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: ["http://localhost:3000", "http://0.0.0.0:3000"], // Add your frontend's origin(s)
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization"],
-      credentials: true, // If your frontend sends cookies or credentials
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
